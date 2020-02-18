c773085c57bc6a938b2d1ca5cb42a70bc5abe588
kareza-best <kareza@qq.com>
2020-02-18 20:31:18 +0800

    Modify re_name.py.

--- re_name.py
+++ re_name.py
@@ -1,11 +1,15 @@
-# 批量删除文件名中的str
-
+# Author: kareza
+# Create time: February 18,2020
+# Fuction: Bulk delete specified string in filename.
+# Example: python3 re_name.py ./mask_640_640 _seg0
 import os
 import sys
 
+FILE_PATH = sys.argv[1]
+STR = sys.argv[2]
+
 if __name__ == "__main__":
-    str = input("请输入你要批量删除文件名中的字符串：")
-    old_names = os.listdir()
+    old_names = os.listdir(FILE_PATH)
     for old_name in old_names:
-        if old_name.find(str):
-            os.rename(old_name, old_name.replace(str,''))
+        if old_name.find(STR):
+            os.rename(FILE_PATH + '/' +old_name, FILE_PATH + '/' + old_name.replace(STR,''))
